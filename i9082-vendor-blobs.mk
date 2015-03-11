1365d4da4e78de246b496b393d3327356849db8f
k2wl <kar2wlan@gmail.com>
2015-03-12 04:08:28 +0530

    pawitp changes for cm12 on i9082: use libwvdrm libs from oppo/msm8974-common
    Signed-off-by: k2wl <kar2wlan@gmail.com>

--- i9082-vendor-blobs.mk
+++ i9082-vendor-blobs.mk
@@ -143,4 +143,10 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/i9082/proprietary/vendor/lib/hw/gralloc.capri.so:system/vendor/lib/hw/gralloc.capri.so \
     vendor/samsung/i9082/proprietary/vendor/lib/hw/hwcomposer.capri.so:system/vendor/lib/hw/hwcomposer.capri.so \
     vendor/samsung/i9082/proprietary/vendor/lib/hw/lights.capri.so:system/vendor/lib/hw/lights.capri.so \
-    vendor/samsung/i9082/proprietary/vendor/lib/hw/power.capri.so:system/vendor/lib/hw/power.capri.so
+    vendor/samsung/i9082/proprietary/vendor/lib/hw/power.capri.so:system/vendor/lib/hw/power.capri.so \
+    vendor/samsung/i9082/proprietary/lib/libdrmdecrypt.so:system/lib/libdrmdecrypt.so \
+    vendor/samsung/i9082/proprietary/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
+    vendor/samsung/i9082/proprietary/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
+    vendor/samsung/i9082/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
+    vendor/samsung/i9082/proprietary/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so \
+    vendor/samsung/i9082/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so
